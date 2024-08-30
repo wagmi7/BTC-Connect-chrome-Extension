2a0fc9229bccd9e542f715af0584896e0555ef96
wagmi7 <0xwagmi7@gmail.com>
2024-08-30 07:32:40 +0000

    extension working

--- btc-connect-extension/src/App.tsx
+++ btc-connect-extension/src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useConnectModal } from "@particle-network/btc-connectkit";
+import "./App.css";
 
 function App() {
+  const { openConnectModal, disconnect } = useConnectModal();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
+      <button onClick={openConnectModal}>Connect wallet</button>
+      <button onClick={disconnect}>Disconnect wallet</button>
     </div>
   );
 }
--- btc-connect-extension/src/index.tsx
+++ btc-connect-extension/src/index.tsx
@@ -1,19 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ConnectProvider,
+  OKXConnector,
+  UnisatConnector,
+  BitgetConnector,
+  TokenPocketConnector,
+  BybitConnector,
+  WizzConnector,
+  XverseConnector,
+} from "@particle-network/btc-connectkit";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import {
+  MerlinTestnet,
+  Merlin,
+  BEVMTestnet,
+  BEVM,
+  MAPProtocolTestnet,
+  MAPProtocol,
+  SatoshiVMTestnet,
+  BSquaredTestnet,
+  Mantle,
+  BitlayerTestnet,
+  BotanixTestnet,
+  PolygonzkEVMCardona,
+} from "@particle-network/chains";
+
+const root = document.createElement("div");
+root.className = "container";
+document.body.appendChild(root);
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
   <React.StrictMode>
-    <App />
+    <ConnectProvider
+      options={{
+        projectId: "7f20d8df-724a-45a9-8f03-20defec66c00", // -
+        clientKey: "cM5l7vR8cIUjOxkhJAmKrBRkCwWPPT70QlnW9fZu", // Retrieved from https://dashboard.particle.network
+        appId: "670e1d70-8207-4504-9165-c8060332e89e", // -
+        aaOptions: {
+          accountContracts: {
+            BTC: [
+              {
+                chainIds: [
+                  MerlinTestnet.id,
+                  Merlin.id,
+                  BEVMTestnet.id,
+                  BEVM.id,
+                  MAPProtocolTestnet.id,
+                  MAPProtocol.id,
+                  SatoshiVMTestnet.id,
+                ],
+                version: "1.0.0",
+              },
+              {
+                chainIds: [
+                  BitlayerTestnet.id,
+                  BotanixTestnet.id,
+                  PolygonzkEVMCardona.id,
+                  BSquaredTestnet.id,
+                  Mantle.id,
+                ],
+                version: "2.0.0",
+              },
+            ],
+          },
+        },
+        walletOptions: {
+          visible: true,
+        },
+      }}
+      connectors={[
+        new UnisatConnector(),
+        new OKXConnector(),
+        new BitgetConnector(),
+        new TokenPocketConnector(),
+        new BybitConnector(),
+        new WizzConnector(),
+        new XverseConnector(),
+      ]}
+    >
+      <App />
+    </ConnectProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
